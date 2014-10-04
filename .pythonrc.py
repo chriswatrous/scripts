13695fab24ba6798bb6f7974333e6ecc8bee5f53
Chris Watrous <chriswatrous@gmail.com>
2014-10-04 00:01:52 -0500

    prevent .pythonrc.py from being run more than once

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,19 +1,22 @@
-import os, sys
-from subprocess import call
-from pprint import *
+if 'pythonrc_guard' not in globals():
+    import os, sys
+    from subprocess import call
+    from pprint import *
 
-def pdir(obj):
-    pprint(dir(obj))
+    def pdir(obj):
+        pprint(dir(obj))
 
-# enable syntax completion
-try:
-    import readline
-except ImportError:
-    print("Module readline not available.")
-else:
-    import rlcompleter
-    readline.parse_and_bind("tab: complete")
+    # enable syntax completion
+    try:
+        import readline
+    except ImportError:
+        print("Module readline not available.")
+    else:
+        import rlcompleter
+        readline.parse_and_bind("tab: complete")
 
-# read local .pythonrc.py
-if os.path.isfile('.pythonrc.py') and os.getcwd() != os.getenv('HOME'):
-    exec(open('.pythonrc.py').read())
+    # read local .pythonrc.py
+    pythonrc_guard = None
+    with open('.pythonrc.py') as f:
+        exec(f.read())
+    del pythonrc_guard
