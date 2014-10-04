60d71df7c2542d102e014eac19aa415ae61cab80
Chris Watrous <chriswatrous@gmail.com>
2014-10-04 00:45:34 -0500

    made update-scripts.py work with all of the scripts in this repo

--- update-scripts.py
+++ update-scripts.py
@@ -6,15 +6,28 @@ from subprocess import call
 # change to the directory of this script
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-installed = os.path.expanduser('~/.bashrc')
-checkout = os.path.realpath('.bashrc')
 
-installed_mtime = os.stat(installed).st_mtime
-checkout_mtime = os.stat(checkout).st_mtime
+scripts = [
+    (os.path.expanduser('~/.bashrc'), os.path.realpath('.bashrc')),
+    (os.path.expanduser('~/.vimrc'), os.path.realpath('.vimrc')),
+    (os.path.expanduser('~/.pythonrc.py'), os.path.realpath('.pythonrc.py')),
+    (os.path.expanduser('~/.octaverc'), os.path.realpath('.octaverc'))]
 
-if installed_mtime > checkout_mtime:
-    print 'Copying %s to %s' % (installed, checkout)
-    call(['cp', '-p', installed, checkout])
-elif installed_mtime < checkout_mtime:
-    print 'Copying %s to %s' % (checkout, installed)
-    call(['cp', '-p', checkout, installed])
+if os.path.isfile(os.path.expanduser('~/.home-cygwin')):
+    scripts.append(('/cygdrive/c/Users/Chris/Documents/AutoHotkey.ahk', os.path.realpath('AutoHotkey.ahk')))
+    scripts.append(('/cygdrive/c/Users/Chris/Documents/AutoHotkey/Lib/PlaceActiveWindow.ahk', os.path.realpath('PlaceActiveWindow.ahk')))
+
+if os.path.isfile(os.path.expanduser('~/.work-cygwin')):
+    scripts.append(('/cygdrive/c/Users/cwatrous/Documents/AutoHotkey.ahk', os.path.realpath('AutoHotkey.ahk')))
+    scripts.append(('/cygdrive/c/Users/cwatrous/Documents/AutoHotkey/Lib/PlaceActiveWindow.ahk', os.path.realpath('PlaceActiveWindow.ahk')))
+
+for installed, checkout in scripts:
+    installed_mtime = os.stat(installed).st_mtime
+    checkout_mtime = os.stat(checkout).st_mtime
+
+    if installed_mtime > checkout_mtime:
+        print 'Copying %s -> %s' % (installed, checkout)
+        call(['cp', '-p', installed, checkout])
+    elif installed_mtime < checkout_mtime:
+        print 'Copying %s -> %s' % (checkout, installed)
+        call(['cp', '-p', checkout, installed])
