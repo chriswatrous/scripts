7765a962b9b57ceb21027e0b772a1c02ab7cd150
Chris Watrous <chriswatrous@gmail.com>
2014-10-04 00:29:51 -0500

    got update-scripts.py to work with .bashrc

--- update-scripts.py
+++ update-scripts.py
@@ -1,4 +1,20 @@
+#! /usr/bin/python
+
 import os
+from subprocess import call
 
-scripts_dir = os.path.dirname(os.path.realpath(__file__))
+# change to the directory of this script
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+installed = os.path.expanduser('~/.bashrc')
+checkout = os.path.realpath('.bashrc')
+
+installed_mtime = os.stat(installed).st_mtime
+checkout_mtime = os.stat(checkout).st_mtime
+
+if installed_mtime > checkout_mtime:
+    print 'Copying %s to %s' % (installed, checkout)
+    call(['cp', '-p', installed, checkout])
+elif installed_mtime < checkout_mtime:
+    print 'Copying %s to %s' % (checkout, installed)
+    call(['cp', '-p', checkout, installed])
