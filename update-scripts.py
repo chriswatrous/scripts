dbeb8f5898a9488ad8a347cd57fefc176abb7c21
Chris Watrous <chriswatrous@gmail.com>
2015-02-22 01:05:53 -0600

    improve the install script; add .pdbrc

--- update-scripts.py
+++ update-scripts.py
@@ -2,39 +2,67 @@
 
 from subprocess import call
 from os import chdir, environ, stat
-from os.path import realpath, expanduser, exists, dirname, join
+from os.path import realpath, expanduser, exists, dirname, join, isdir
 
 chdir(dirname(realpath(__file__)))
 
-scripts = [[expanduser('~/.bashrc'), realpath('.bashrc')],
-           [expanduser('~/.pythonrc.py'), realpath('.pythonrc.py')],
-           [expanduser('~/.octaverc'), realpath('.octaverc')]]
+#scripts = [[expanduser('~/.bashrc'), realpath('.bashrc')],
+           #[expanduser('~/.pythonrc.py'), realpath('.pythonrc.py')],
+           #[expanduser('~/.octaverc'), realpath('.octaverc')]]
 
-if exists('/cygdrive'):
-    winhome = environ['USERPROFILE']
-    winhome = join('/cygdrive/c/Users', environ['USER'])
-    scripts.extend(
-        [[join(winhome, 'Documents/AutoHotkey.ahk'), realpath('AutoHotkey.ahk')],
-         [join(winhome, 'Documents/AutoHotkey/Lib/PlaceActiveWindow.ahk'), realpath('PlaceActiveWindow.ahk')],
-         [expanduser('~/.vimrc'), join(winhome, '_vimrc'), '.vimrc']])
-else:
-    scripts.append([expanduser('~/.vimrc'), realpath('.vimrc')])
+winhome = join('/cygdrive/c/Users', environ['USER'])
 
-def add_if_exists(install_dir, filename):
-    ipath = join(expanduser(install_dir), filename)
-    if exists(ipath):
-        scripts.append([ipath, filename])
+scripts = {'.bashrc':               ['~/.bashrc'],
+           '.pythonrc.py':          ['~/.pythonrc.py'],
+           '.octaverc':             ['~/.octaverc'],
+           '.ghci':                 ['~/.ghci'],
+           '.pdbrc':                ['~/.pdbrc'],
+           'AutoHotkey.ahk':        [join(winhome, 'Documents/AutoHotkey.ahk')],
+           'PlaceActiveWindow.ahk': [join(winhome, 'Documents/AutoHotkey/Lib/PlaceActiveWindow.ahk')],
+           '.vimrc':                ['~/.vimrc', join(winhome, '_vimrc')],
+           'lxde-rc.xml':           ['~/.config/openbox/lxde-rc.xml']}
 
-add_if_exists('~/.config/openbox', 'lxde-rc.xml')
-add_if_exists('~', '.ghci')
+def main():
+    print "Checking for installed files:"
 
-for files in scripts:
-    mtimes = [stat(x).st_mtime for x in files]
-    if len(set(mtimes)) > 1:
-        i = mtimes.index(max(mtimes))
-        source = files[i]
-        files.remove(source)
-        for f in files:
-            print 'Copying %s -> %s' % (source, f)
-            call(['cp', '-p', source, f])
+    for name in scripts:
+        scripts[name] = map(expanduser, scripts[name])
 
+    for name in scripts:
+        for path in list(scripts[name]):
+            if not exists(path):
+                print "    Installied file not found: '{}'".format(path)
+                scripts[name].remove(path)
+            if isdir(path):
+                print "    Path is a directory: '{}'".format(path)
+                scripts[name].remove(path)
+
+
+    for name in scripts.keys():
+        if scripts[name] == []:
+            print "    Skipping '{}': no installed files found".format(name)
+            del scripts[name]
+
+    print "Updating files:"
+    updated = False
+    for name in scripts:
+        files = [name] + scripts[name]
+        mtimes = [stat(x).st_mtime for x in files]
+        if len(set(mtimes)) > 1:
+            updated = True
+            i = mtimes.index(max(mtimes))
+            source = files[i]
+            files.remove(source)
+            for f in files:
+                print '    Copying %s -> %s' % (source, f)
+                call(['cp', '-p', source, f])
+
+    if not updated:
+        print '    Nothing to update.'
+
+
+
+
+
+if __name__ == '__main__':
+    main()
